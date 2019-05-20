de9fc65cde9d87343bdd72b20d192ae293e53d20
poignant-minifier-0983 <davidfano@gmail.com>
2019-05-20 01:30:08 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,9 +1,9 @@
 def happy_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
     if age <= 12
-      print "Happy Birthday #{kids_name}! You are now #{age} years old!"
+      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
     else
-      print "You are too old for this."
+      puts "You are too old for this."
     end
   end
 end
